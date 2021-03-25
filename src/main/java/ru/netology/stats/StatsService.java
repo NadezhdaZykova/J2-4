7ad49d77964e3ec996be048c255de88fab8d913f
NadezhdaZykova <xilda777@gmail.com>
2021-03-25 14:30:39 +0400

    Fixed

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,67 +1,76 @@
 package ru.netology.stats;
 
 public class StatsService {
-    long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-    long[] month = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    long average = 15;
 
     public long calculateSum(long[] purchases) {
         long sum = 0;
         for (long purchase : purchases) {
-            // аналог sum = sum + purchase;
             sum += purchase;
         }
         return sum;
     }
 
-    public long calculateAverage(long[] purchases) {
-        long sum = 0;
+    public long calculateAvg(long[] purchases) {
+        long sum = calculateSum(purchases);
+        long average = sum / purchases.length;
+
+        return average;
+    }
+
+    public long findLastMonthWhenMaxSales(long[] purchases) {
+        long max = purchases[0];
         for (long purchase : purchases) {
-            // аналог sum = sum + purchase;
-            sum += purchase;
-        }
-        return sum / month.length;
-
-    }
-
-    public long findMonthWhenMaxSales(long[] month) {
-        int index = 0; // начинаем с нуля
-        for (int i = 1; i < month.length; i++) {
-            if (purchases[index] < purchases[i])
-                index = i;
-            else
-                index = index;
-            System.out.println("Номер месяца: " + month[index]);
-        }
-        return month[index];
-    }
-
-    public long findMonthWhenMinSales(long[] month) {
-        int index = 0; // начинаем с нуля
-        for (int i = 1; i < month.length; i++) {
-            if (purchases[index] > purchases[i])
-                index = i;
-            else
-                index = index;
-            System.out.println("Номер месяца: " + month[index]);
-        }
-        return month[index];
-    }
-
-    public long belowAverage(long[] purchases) {
-        long currentMin = purchases[0];
-        for (long purchase : purchases) {
-            if (currentMin > average) {
-                currentMin = purchase;
+            if (purchase > max) {
+                max = purchase;
             }
-        } return currentMin;
-    }
-    public long overAverage(long[] purchases) {
-        long currentMin = purchases[0];
+        }
+        long monthCount = 0;
+        long monthMax = 0;
         for (long purchase : purchases) {
-            if (currentMin < average) {
-                currentMin = purchase;
+            monthCount++;
+            if (purchase == max) {
+                monthMax = monthCount;
             }
-        } return currentMin;
+        }
+        return monthMax;
+    }
+
+    public long findMonthWhenMinSales(long[] purchases) {
+        long min = purchases[0];
+        for (long purchase : purchases) {
+            if (purchase < min) {
+                min = purchase;
+            }
+        }
+        long monthCount = 0;
+        long monthMin = 0;
+        for (long purchase : purchases) {
+            monthCount++;
+            if (purchase == min) {
+                monthMin = monthCount;
+            }
+        }
+        return monthMin;
+    }
+
+    public long belowAverageSales(long[] items) {
+        long monthCount = 0;
+        long avg = calculateAvg(items);
+        for (long item : items) {
+            if (item > avg) {
+                monthCount++;
+            }
+        }
+        return monthCount;
+    }
+
+    public long overAverageSales(long[] items) {
+        long monthCount = 0;
+        long avg = calculateSum(items) / items.length;
+        for (long item : items) {
+            if (item < avg) {
+                monthCount++;
+            }
+        }  return monthCount;
     }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -6,52 +6,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
     long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-    long[] month = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    StatsService service = new StatsService();
 
     @Test
     void sumSales() {
-
-        StatsService service = new StatsService();
         long expected = 180;
         long actual = service.calculateSum(purchases);
         assertEquals(expected, actual);
     }
     @Test
     void averageSales() {
-
-        StatsService service = new StatsService();
         long expected = 15;
-        long actual = service.calculateAverage(purchases);
+        long actual = service.calculateAvg(purchases);
         assertEquals(expected, actual);
     }
     @Test
     void findMonthWhenMaxSales() {
-        StatsService service = new StatsService();
         long expected = 8;
-        long actual = service.findMonthWhenMaxSales(month);
+        long actual = service.findLastMonthWhenMaxSales(purchases);
         assertEquals(expected, actual);
     }
     @Test
     void findMonthWhenMinSales() {
-        StatsService service = new StatsService();
         long expected = 9;
-        long actual = service.findMonthWhenMinSales(month);
+        long actual = service.findMonthWhenMinSales(purchases);
         assertEquals(expected, actual);
     }
     @Test
     void belowAverage() {
-        StatsService service = new StatsService();
         long expected = 5;
-        long actual = service.belowAverage(purchases);
+        long actual = service.belowAverageSales(purchases);
 
         assertEquals(expected, actual);
     }
     @Test
     void overAverage() {
-        StatsService service = new StatsService();
         long expected = 5;
-        long actual = service.belowAverage(purchases);
-
+        long actual = service.overAverageSales(purchases);
         assertEquals(expected, actual);
     }
 }
